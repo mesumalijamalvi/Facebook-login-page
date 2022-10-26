71392ad40e36ca262d1783f370de9560ce6bc0fa
mesumalijamalvi <mesamjamalvi592@gmail.com>
2022-10-26 02:41:02 -0700

    Facebook login page

---
+++ app.js
@@ -0,0 +1,23 @@
+//  let m = document.getElementById("name") 
+//  let n = document.getElementById("last") 
+//  let o = document.getElementById("password")
+ 
+//  let button = document.getElementById("btn")
+
+//  button.addEventListener('click',()=>{
+//     document.write( "first Name is =",m.value +"   "+"last name is =",
+//      n.value+"   "+ "password is", o.value);
+//  })
+
+
+
+let v = document.getElementById("name");
+let o = document.getElementById("last");
+let j = document.getElementById("password");
+let button = document.getElementById("btn");
+
+button.addEventListener("click",()=>{
+  document.write("1st name is-",v.value+"  "+"last name is-",o.value+ "  " +"password is- ",j.value)
+})
+
+
